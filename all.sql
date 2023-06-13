bb81e437415273ddb4122d62cee0a70d6e0e30de
hajarkdidar <84786872+mohamedHakmaoui@users.noreply.github.com>
2023-06-13 13:21:42 +0200

    modification de trigger ajout de fonction

--- all.sql
+++ all.sql
@@ -1,3 +1,28 @@
+CREATE OR REPLACE FUNCTION check_unique_president()
+RETURNS TRIGGER AS $$
+DECLARE
+    total INTEGER;
+BEGIN
+    IF NEW.type = 'ADMINISTRATEUR_UNIV' or NEW.type ='PRESIDENT'  THEN
+        SELECT COUNT(*) INTO total FROM users WHERE type = New.type;
+
+        IF total > 0 THEN
+            RAISE EXCEPTION 'There can be only one type of this  user in UNIVERSITY ABMS.';
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER unique_president
+BEFORE INSERT OR UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION check_unique_president();
+
+
+----------------------------------
+----------------------------------
 ---------pour etre sur que instruction delete ne vient pas a ma base de donne j'ai developper un trriger
 CREATE OR REPLACE FUNCTION bloque_suppression_enseignant()
 RETURNS TRIGGER AS $$
@@ -287,73 +312,78 @@ CREATE or replace TRIGGER visa_uae_trigger
 AFTER UPDATE ON interventions
 FOR EACH ROW
 EXECUTE FUNCTION update_paiement();
------------------------------
-------------------------------
-CREATE OR REPLACE FUNCTION update_paiement()
+---------------------------
+-----------------------
+CREATE OR REPLACE FUNCTION delete_paiement_trigger()
+
 RETURNS TRIGGER AS $$
+
 DECLARE
-    existing_paiement paiement%ROWTYPE;
-    grade_info grade%ROWTYPE;
-    etab_exists BOOLEAN;
+volume Integer;
     same_etab BOOLEAN;
-    VH1 INTEGER;
     total_hours INTEGER;
-    montant_brut INTEGER;
-    montant_net INTEGER;
+    grade_info grade%ROWTYPE;
+
 BEGIN
-    IF (old.visa_uae = false and NEW.Visa_uae= true ) THEN
-        SELECT * INTO existing_paiement
-        FROM paiements
-        WHERE id_Intervenant = NEW.id_Intervenant
-        AND id_etab = NEW.id_Etab
-        AND Annee_Univ = NEW.Annee_univ
-        AND Semestre = NEW.Semestre;
+    IF (OLD.Visa_uae =true AND NEW.Visa_uae = false) THEN
 
-        IF FOUND THEN
-        VH1 := existing_paiement.VH + NEW.Nbr_heures;
+        SELECT NEW.id_etab = Etablissement INTO same_etab
+        FROM enseignants
+        WHERE id = NEW.id_Intervenant;
 
-    
-        UPDATE paiements
-SET VH = VH1,
-    Brut = existing_paiement.Brut + calcule_brut(NEW.Nbr_heures, existing_paiement.Taux_H),
-    Net = existing_paiement.Net + calcule_net(calcule_brut(NEW.Nbr_heures, existing_paiement.Taux_H), existing_paiement.IR)
-WHERE id = existing_paiement.id;
+        IF (NOT same_etab) THEN
+        select VH into volume from paiements
+        WHERE id_Intervenant = OLD.id_Intervenant
+                    AND id_etab = OLD.id_Etab
+                    AND Annee_Univ = OLD.Annee_univ
+                    AND Semestre = OLD.Semestre;
 
-
+            IF (volume - NEW.Nbr_heures > 0) THEN
+                UPDATE paiements
+                SET VH = volume - NEW.Nbr_heures,
+                    Brut = Brut - calcule_brut(NEW.Nbr_heures, Taux_H),
+                    Net = Net - calcule_net(calcule_brut(NEW.Nbr_heures, Taux_H), IR)
+                WHERE id_Intervenant = OLD.id_Intervenant
+                    AND id_etab = OLD.id_Etab
+                    AND Annee_Univ = OLD.Annee_univ
+                    AND Semestre = OLD.Semestre;
+            ELSE
+                DELETE FROM paiements
+                WHERE id_Intervenant = OLD.id_Intervenant
+                    AND id_etab = OLD.id_Etab
+                    AND Annee_Univ = OLD.Annee_univ
+                    AND Semestre = OLD.Semestre;
+            END IF;
         ELSE
             SELECT * INTO grade_info
             FROM grade
-            WHERE id = (SELECT id_Grade FROM enseignants WHERE id = NEW.id_Intervenant);
+            WHERE id = (SELECT id_Grade FROM enseignant WHERE id = NEW.id_Intervenant);
 
-            SELECT Etablissement = NEW.id_etab INTO same_etab
-            FROM enseignants
-            WHERE id = NEW.id_Intervenant;
-
-
-            IF (same_etab = FALSE) then
-                INSERT INTO paiements (id_Intervenant, id_etab, VH, Taux_H, Brut, IR, Net, Annee_Univ, Semestre)
-                VALUES (NEW.id_Intervenant, NEW.id_etab, NEW.Nbr_heures, grade_info.Taux_horaire_vacation,
-                        calcule_brut(NEW.Nbr_heures, grade_info.Taux_horaire_vacation), 38,
-                        calcule_net(calcule_brut(NEW.Nbr_heures, grade_info.Taux_horaire_vacation), 38),
-                        NEW.Annee_univ, NEW.Semestre);
-            ELSE
-                SELECT SUM(Nbr_heures) INTO total_hours
-                FROM interventions
-                WHERE id_Intervenant = NEW.id_Intervenant
+            SELECT SUM(Nbr_heures) INTO total_hours
+            FROM interventions
+            WHERE id_Intervenant = NEW.id_Intervenant
                 AND id_Etab = NEW.id_Etab
                 AND Annee_univ = NEW.Annee_univ
                 AND Semestre = NEW.Semestre
-                and visa_uae=true;
+                AND Visa_uae = true;
 
-                IF total_hours > grade_info.charge_statutaire THEN
-                total_hours := total_hours - grade_info.charge_statutaire;
-                    montant_brut := calcule_brut(total_hours, grade_info.Taux_horaire_vacation);
-                    montant_net := calcule_net(montant_brut, 38);
-                    
-                    INSERT INTO paiements (id_Intervenant, id_etab, VH, Taux_H, Brut, IR, Net, Annee_Univ, Semestre)
-                    VALUES (NEW.id_Intervenant, NEW.id_etab, total_hours, grade_info.Taux_horaire_vacation,
-                            montant_brut, 38, montant_net, NEW.Annee_univ, NEW.Semestre);
-                END IF;
+           
+            IF (total_hours > grade_info.charge_statutaire) THEN 
+            total_hours:= total_hours - grade_info.charge_statutaire;
+                UPDATE paiements
+                SET VH = total_hours,
+                    Brut = calcule_brut(total_hours, Taux_H),
+                    Net = calcule_net(calcule_brut(total_hours, Taux_H), IR)
+                WHERE id_Intervenant = OLD.id_Intervenant
+                    AND id_etab = OLD.id_Etab
+                    AND Annee_Univ = OLD.Annee_univ
+                    AND Semestre = OLD.Semestre;
+            ELSE
+                DELETE FROM paiements
+                WHERE id_Intervenant = OLD.id_Intervenant
+                    AND id_etab = OLD.id_Etab
+                    AND Annee_Univ = OLD.Annee_univ
+                    AND Semestre = OLD.Semestre;
             END IF;
         END IF;
     END IF;
